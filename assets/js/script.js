98d6332fe6ac76bd6fc7f37e8ed1b69cb0a83a9a
Christopher Matthew <christopher.s.matthew@gmail.com>
2024-11-22 15:20:32 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+/*1. Initialize the application
+    a. Create an empty list to store todos
+
+2. Display the main menu
+    a. Show options: Add Todo, View Todos, Update Todo, Delete Todo, Exit
+
+3. Handle user input
+    a. If user chooses Add Todo:
+        i. Prompt for todo description
+        ii. Add the new todo to the list
+    b. If user chooses View Todos:
+        i. Display all todos with their indices
+    c. If user chooses Update Todo:
+        i. Prompt for the index of the todo to update
+        ii. Prompt for the new description
+        iii. Update the todo at the specified index
+    d. If user chooses Delete Todo:
+        i. Prompt for the index of the todo to delete
+        ii. Remove the todo at the specified index
+    e. If user chooses Exit:
+        i. End the application
+
+4. Loop back to the main menu until the user chooses to exit
+*/
+
